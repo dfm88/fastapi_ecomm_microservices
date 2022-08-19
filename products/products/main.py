66f4df2f88e4c37dc839837d8cdb5c9192f696f5
dfm88 <diegof.university@gmail.com>
2022-08-19 14:36:01 +0200

    entrypoint for each service

---
+++ products/products/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(
+    
+)
+
+
+@app.get('/')
+async def main():
+    return "Product is running"
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8003)
